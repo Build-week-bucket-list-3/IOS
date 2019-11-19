19077f3ff16cdb2a39d7e0870be4ddaa950be636
Gi Pyo Kim <gipyok@gmail.com>
2019-11-18 23:36:19 -0800

    Added journal and media views

---
+++ BucketList/BucketList/BLIDetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  BLIDetailViewController.swift
+//  BucketList
+//
+//  Created by Gi Pyo Kim on 11/18/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class BLIDetailViewController: UIViewController {
+
+    @IBOutlet weak var journalMediaSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var journalViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mediaViewLeadingConstraint: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Make journal view show by default
+        journalMediaSegmentedControl.selectedSegmentIndex = 0
+        journalViewLeadingConstraint.constant = 0
+        mediaViewLeadingConstraint.constant = 300
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func switchJournalMedia(_ sender: Any) {
+        
+        if journalMediaSegmentedControl.selectedSegmentIndex == 0 {
+            journalViewLeadingConstraint.constant = 0
+            mediaViewLeadingConstraint.constant = 300
+        } else {
+            journalViewLeadingConstraint.constant = -300
+            mediaViewLeadingConstraint.constant = 0
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+}
